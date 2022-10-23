71bfb7599bb3a2d110472ef94dcce335cb38de8f
Daniel Russom <daniel.russom00@gmail.com>
2022-10-23 22:49:45 +0100

    Init

---
+++ Project Euler Exercises/Project Euler Exercises.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Project_Euler_Exercises.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
