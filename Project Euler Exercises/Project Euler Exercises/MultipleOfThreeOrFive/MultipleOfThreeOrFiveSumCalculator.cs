cf61e90bb184cba62b191d86fdd4d6063ad31700
Daniel Russom <daniel.russom00@gmail.com>
2022-11-01 22:41:02 +0000

    Up to 9 refactor

--- Project Euler Exercises/Project Euler Exercises/MultipleOfThreeOrFive/MultipleOfThreeOrFiveSumCalculator.cs
+++ Project Euler Exercises/Project Euler Exercises/MultipleOfThreeOrFive/MultipleOfThreeOrFiveSumCalculator.cs
@@ -2,11 +2,19 @@
 {
     public class MultipleOfThreeOrFiveSumCalculator
     {
-        private readonly int[] values = new int[]{9, 6, 5, 3};
+        private readonly int[] values = new int[]{5};
 
         public int SumTo(int limit)
         {
             var result = 0;
+
+            for(int count = 1; count <= limit; count++)
+            {
+                if(count % 3 == 0)
+                {
+                    result += count;
+                }
+            }
 
             foreach (var value in values)
             {
