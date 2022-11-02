e0ef6eefbcb03d9955d16afc50f6322dde1c704a
Daniel Russom <daniel.russom00@gmail.com>
2022-11-02 21:38:54 +0000

    Sum Even Fibonacci numbers : to 2

--- Project Euler Exercises/Project Euler Exercises.Test/EvenFibonacci/EvenFibonacciNumberCalculatorShould.cs
+++ Project Euler Exercises/Project Euler Exercises.Test/EvenFibonacci/EvenFibonacciNumberCalculatorShould.cs
@@ -16,5 +16,16 @@ namespace Project_Euler_Exercises.Test.EvenFibonacci
 
             Assert.AreEqual(0, result);
         }
+
+        [TestMethod]
+        public void Calculate_2()
+        {
+            var underTest = new EvenFibonacciNumberCalculator();
+            var limit = 2;
+
+            var result = underTest.SumEvenNumbersTo(limit);
+
+            Assert.AreEqual(2, result);
+        }
     }
 }
