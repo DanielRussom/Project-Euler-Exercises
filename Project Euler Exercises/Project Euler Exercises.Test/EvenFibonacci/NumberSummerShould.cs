c532e60384dbda609ee743220d4a0f8536504e76
Daniel Russom <daniel.russom00@gmail.com>
2022-11-05 15:30:51 +0000

    Sum list with two even numbers - fix rule of 3 on tests

--- Project Euler Exercises/Project Euler Exercises.Test/EvenFibonacci/NumberSummerShould.cs
+++ Project Euler Exercises/Project Euler Exercises.Test/EvenFibonacci/NumberSummerShould.cs
@@ -8,37 +8,15 @@ namespace Project_Euler_Exercises.Test.EvenFibonacci
     public class NumberSummerShould
     {
         [TestMethod]
-        public void Calculate_0_from_empty_list()
+        [DataRow (0, new int[]{ })]
+        [DataRow(2, new int[] { 2 })]
+        [DataRow(4, new int[] { 2, 2 })]
+        [DataRow(10, new int[] { 8, 2 })]
+        public void Sum_all_numbers_in_list(int expected, int[] input)
         {
             var underTest = new NumberSummer();
-            var expected = 0;
-            var input = new List<int>();
 
-            var result = underTest.SumEven(input);
-
-            Assert.AreEqual(expected, result);
-        }
-
-        [TestMethod]
-        public void Calculate_2_for_list_containing_only_2()
-        {
-            var underTest = new NumberSummer();
-            var expected = 2;
-            var input = new List<int> { 2 };
-
-            var result = underTest.SumEven(input);
-
-            Assert.AreEqual(expected, result);
-        }
-
-        [TestMethod]
-        public void Calculate_4_for_list_containing_2_twice()
-        {
-            var underTest = new NumberSummer();
-            var expected = 4;
-            var input = new List<int> { 2, 2 };
-
-            var result = underTest.SumEven(input);
+            var result = underTest.SumEven(new List<int>(input));
 
             Assert.AreEqual(expected, result);
         }
