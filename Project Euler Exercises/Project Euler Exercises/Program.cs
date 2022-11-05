08de7ebd7ea5fbd3556e29e8a3395a5c2813ed88
Daniel Russom <daniel.russom00@gmail.com>
2022-11-05 15:45:12 +0000

    Add Even Fibonacci to Program.cs

--- Project Euler Exercises/Project Euler Exercises/Program.cs
+++ Project Euler Exercises/Project Euler Exercises/Program.cs
@@ -1,6 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using Project_Euler_Exercises.MultiplesOfThreeOrFive;
 
 Console.WriteLine("Hello, World!");
+
+// *** Exercise 2 ***
+//var fibonacciGenerator = new FibonacciNumberGenerator();
+//var summer = new NumberSummer();
+//var fibonacciEngine = new SumEvenFibonacciNumbersEngine(fibonacciGenerator, summer);
+//Console.WriteLine(fibonacciEngine.SumToLimit(4000000));
+
+// *** Exercise 1 ***
 //var calculator = new MultipleOfThreeOrFiveSumCalculator();
 //Console.WriteLine(calculator.SumTo(1000));
